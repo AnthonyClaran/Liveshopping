0573630f0314c66941b7f9af507f90b8c95b95a5
AnthonyClaran <anthonyrandrianantoandro@gmail.com>
2025-10-18 01:27:35 +0300

    Update bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -8,5 +8,6 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
