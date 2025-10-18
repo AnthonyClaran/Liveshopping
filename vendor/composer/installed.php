c1266f0ba3c3813e9121944bdf25379733e4508b
AnthonyClaran <157488965+AnthonyClaran@users.noreply.github.com>
2025-10-18 16:52:29 +0300

    update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c3d520b57b6c266a20717ed70452b5c6210aeb66',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c3d520b57b6c266a20717ed70452b5c6210aeb66',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
